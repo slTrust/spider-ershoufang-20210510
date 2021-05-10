e897822f8b85bfe58e3a07f98f6e125c8e93c112
slTrust <25893245+slTrust@users.noreply.github.com>
2021-05-10 21:38:07 +0800

    Update sql.sql

--- ershoufang/db/sql.sql
+++ ershoufang/db/sql.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS lianjia
-  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+  CHARACTER SET utf8mb4
 
 create table app_house(
   id bigint primary key auto_increment,
